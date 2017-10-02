4eb843df38ca729f0e8059d09f337d17fc416186
kenya-sk <kenya0sakka@gmail.com>
2017-10-02 18:17:01 +0900

    modify

--- code/batch.py
+++ code/batch.py
@@ -8,6 +8,7 @@ import cv2
 import re
 import draw_opt
 import get_frameList
+import plot_kernelDensity
 
 def batch_processing():
     #--------------------------------------------------
@@ -22,7 +23,7 @@ def batch_processing():
         sys.exit(1)
 
     #get flush event time list
-    flushList = get_frameList.get_frameList("./flushSec.txt")
+    #flushList = get_frameList.get_frameList("./flushSec.txt")
 
     #-------------------------------------------------
     #processing all data
@@ -31,12 +32,16 @@ def batch_processing():
     fileList = os.listdir(direc)
     pattern = r'^(?!._).*(.mp4)$'
     repattern = re.compile(pattern)
+    norDensity_arr = np.zeros((720, 1280))
     count = 0
     for file_name in fileList:
         if re.search(repattern,file_name):
             inputFile = direc + '/' + file_name
-            draw_opt.main(inputFile,flushList[count])
-            count += 1
+            #draw_opt.main(inputFile,flushList[count])
+            #count += 1
+            norDensity_arr = plot_kernelDensity.get_binaryData(inputFile)
+            np.save("../data/dense/dens_{}.npy".format(file_name), norDensity_arr)
+            print("success dens_{}.npy\n".format(file_name))
 
 if __name__ == '__main__':
      batch_processing()
