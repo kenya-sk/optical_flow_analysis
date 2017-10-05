4ab5e411564101e59712c32cfba75aeba94ef05b
kenya-sk <kenya0sakka@gmail.com>
2017-10-05 19:32:53 +0900

    must change mean/var calculation

--- code/draw_opt.py
+++ code/draw_opt.py
@@ -11,46 +11,32 @@ import argparse
 import get_frameList
 import plot_graph
 
+FPS = None
 HEIGHT = None
 WIDTH = None
 
-def draw_flow(img, gray, maskFlow, typeNum=1):
+def set_capture(filePath):
+    cap = cv2.VideoCapture(filePath)
+    fourcc = int(cv2.VideoWriter_fourcc(*'avc1'))
+    fps = int(cap.get(cv2.CAP_PROP_FPS)/2)
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    totalFrame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    return cap, fourcc, fps, height, width, totalFrame
+
+def draw_flow(img, maskFlow, step=8):
     '''
     describe optical flow in masked area
     img: original image
-    gray: gray scale image
-    flow: optical flow value array
-    typeNum: 1: one color 2: two color
+    flow: masked optical flow value array
     '''
     x, y = np.mgrid[step/2:WIDTH:step, step/2:HEIGHT:step].reshape(2,-1).astype(int)
-    fx, fy = flow[y, x].T
-    # change amount array
-    opt_size = np.empty(len(fx))
-    for i in range(len(fx)):
-        opt_size[i] = (fx[i]**2 + fy[i]**2)**0.5
+    fx, fy = maskFlow[y, x].T
 
     lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines)
-    # 1: only one color(red)
-    if typeNum == 1:
-        col = (0, 0, 255)
-        for (x1, y1), (x2, y2) in lines:
-            cv2.line(img, (x1, y1), (x2, y2), col, 1)
-    # 2: decide accleration(red)/decleration(blue)
-    elif typeNum == 2:
-        i = 0
-        prev = 0
-        for (x1, y1), (x2, y2) in lines:
-            if opt_size[i] > prev:
-                col = (0, 0, 255)
-            else:
-                col = (255, 0, 0)
-            cv2.line(img, (x1, y1), (x2, y2), col, 1)
-            if (i + 1) % width == 1:
-                prev = 0
-            else:
-                prev = opt_size[i]
-            i += 1
+    for (x1, y1), (x2, y2) in lines:
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
     return img
 
 def show_gage(pointList, num):
@@ -61,71 +47,52 @@ def show_gage(pointList, num):
     '''
     if num in pointList:
         numIdx = pointList.index(num) + 1
-        sys.stderr.write('\rWriting Rate:[{0}{1}] {2}%'.format(
-            '*' * numIdx * 2, ' ' * (20 - numIdx), numIdx * 10))
-
-def calc_flow(prevGray, gray):
-    flow = cv2.calcOpticalFlowFarneback(prevGray,gray,None,0.5,3,15,3,5,1.2,0)
-    return flow
+        sys.stderr.write('\rWriting Rate:[{0}] {1}%'.format(
+            '*' * numIdx, numIdx * 10))
 
 
-def calc_mean(maskFlow):
-    '''
-    calculate mean (optical flow value) in masked area
-    '''
-    mean = np.mean(maskFlow)
-    return mean
-
-
-def calc_val(maskFlow):
-    '''
-    calculate variance (optical flow value) in masked area
-    '''
-    var = np.var(maskFlow)
-    return var
-
-def load_mask(filepath):
-    mask = cv2.imread(filepath, 0)
-    mask = cv2.merge((mask, mask, mask))
-    mask[mask==255] = 1
-    return mask
-
-def main(filePath):
+def density_flow(filePath, output=False):
     global HEIGHT
     global WIDTH
-    #-------------------------------------------------------
-    # pre processing
-    #------------------------------------------------------
-    # capture movie and data
-    cap = cv2.VideoCapture(filePath)
-    fourcc = int(cv2.VideoWriter_fourcc(*'avc1'))
-    fps = int(cap.get(cv2.CAP_PROP_FPS)/2)
-    HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    totalFrame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    # preprocessing for loding gage
-    point = int(totalFrame / 20)
-    pointList = []
-    for i in range(point, totalFrame, point):
-        pointList.append(i)
-    sys.stderr.write('\rWriting Rate:[{0}] {1}%'.format(' ' * 20, 0))
-    # output movie
-    #out = cv2.VideoWriter('../movie/out_' + filePath.split('/')[-1],
-    #fourcc, fps, (WIDTH, HEIGHT))
-    # initial frame
-    cap.set(cv2.CAP_PROP_POS_MSEC, 3 * 1000)
-    ret, prev = cap.read()
-    prevGray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-    # average value list per frame
-    meanList = []
-    # variance value list per frame
-    varList = []
-    #load mask image
-    mask = load_mask('../image/image_data/mask.png')
+    global FPS
+
+    def init_gage(totalFrame):
+        point = int(totalFrame / 10)
+        pointList = []
+        for i in range(point, totalFrame, point):
+            pointList.append(i)
+        sys.stderr.write('\rWriting Rate:[{0}] {1}%'.format(' ', 0))
+        return pointList
+
+    def load_mask(filepath):
+        mask = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.merge((mask, mask))
+        mask[mask==255] = 1
+        return mask
+
+    def calc_flow(prevGray, gray):
+        flow = cv2.calcOpticalFlowFarneback(prevGray,gray,None,0.5,3,15,3,5,1.2,0)
+        return flow
 
     #-------------------------------------------------------
+    # preprocessing
+    #-------------------------------------------------------
+    cap, fourcc, FPS, HEIGHT, WIDTH, totalFrame = set_capture(filePath)
+    pointList = init_gage(totalFrame)
+    if output:
+        out = cv2.VideoWriter('../movie/out_' + filePath.split('/')[-1],
+        fourcc, FPS, (WIDTH, HEIGHT))
+    else:
+        pass
+    cap.set(cv2.CAP_PROP_POS_MSEC, 3 * 1000)    # initial frame
+    ret, prev = cap.read()
+    prevGray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+    meanList = []
+    varList = []
+    mask = load_mask('../image/image_data/mask.png')
+    #-------------------------------------------------------
     # caluculate optical flow per frame
-    #------------------------------------------------------
+    #-------------------------------------------------------
     frameNum = 0
     while (cap.isOpened()):
         ret,img = cap.read()
@@ -133,25 +100,36 @@ def main(filePath):
         if ret == True:
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
             flow = calc_flow(prevGray, gray)
-            prevGray = gray
             maskFlow = flow*mask
-            mean = calc_mean(maskFlow)
+            mean = np.mean(maskFlow)
             meanList.append(mean)
-            var = calc_val(maskFlow)
+            var = np.var(maskFlow)
             varList.append(var)
-            # make optical flow image
-            #flow_img = draw_flow(img, gray, maskFlow, 1)
-            # restore and display
-            #out.write(flow_img)
+            if output:
+                flow_img = draw_flow(img, maskFlow, 8)
+                out.write(flow_img)
+                cv2.imshow("flow_img", flow_img)
+            else:
+                pass
             show_gage(pointList,frameNum)
+            prevGray = gray
             if cv2.waitKey(1)&0xff == 27:
+                break
+            if frameNum == 150:
                 break
         else:
             break
-    plot_graph.mean_val_plot(meanList, valList, filePath, frameList, FPS)
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+    print("meanList = {}".format(meanList))
+    print("varList = {}".format(varList))
+
+    return meanList, varList
+
+def main(filePath):
+    meanList, varList = density_flow(filePath, True)
+    plot_graph.mean_val_plot(meanList, varList, filePath, FPS)
 
 
 def make_parse():
@@ -165,11 +143,10 @@ def make_parse():
                         epilog='end',
                         add_help=True,
                         )
-
     parser.add_argument('Arg1: input file path',help='string',type=argparse.FileType('r'))
-
     args = parser.parse_args()
-"""
+
+
 if __name__ == '__main__':
     make_parse()
     start = time.time()
@@ -182,4 +159,3 @@ if __name__ == '__main__':
     minute = int(elapsed_time/60)
     sec = int(elapsed_time - minute*60)
     print('\nelapsed_time: {0}分{1}秒'.format(minute,sec))
-"""
--- code/plot_graph.py
+++ code/plot_graph.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def mean_val_plot(meanList, valList, filePath, frameList, fps):
+def mean_val_plot(meanList, valList, filePath, fps):
     '''
     plot mean and variance
     meanList: mean value list
@@ -31,7 +31,7 @@ def mean_val_plot(meanList, valList, filePath, frameList, fps):
     plt.xlim(0.0, 300.0)
     plt.ylim(0.0, 2.0)
     plt.plot(meanX, meanList)
-    fill_region(frameList)
+    #fill_region(frameList)
     # variance list
     valX = []
     for i in range(len(valList)):
@@ -43,7 +43,7 @@ def mean_val_plot(meanList, valList, filePath, frameList, fps):
     plt.xlim(0.0, 300.0)
     plt.ylim(0, 10)
     plt.plot(valX, valList)
-    fill_region(frameList)
+    #fill_region(frameList)
     fileName = filePath.split('/')[-1].split('.')[0] + '.png'
     plt.savefig('../image/20170429_2/' + fileName)
     print('\n'+fileName + ' graph success !\n')
