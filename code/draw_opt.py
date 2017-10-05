083dd8a1f821429426693e04040991395f0d572d
kenya-sk <kenya0sakka@gmail.com>
2017-10-05 14:18:31 +0900

    modify mask calculation

--- code/draw_opt.py
+++ code/draw_opt.py
@@ -11,23 +11,18 @@ import argparse
 import get_frameList
 import plot_graph
 
-FOURCC = None
-FPS = None
 HEIGHT = None
 WIDTH = None
-TOTALFRAME = None
 
-
-def partical_draw_flow(img, gray, flow, active, typeNum=1):
+def draw_flow(img, gray, maskFlow, typeNum=1):
     '''
-    describe optical flow in target area
+    describe optical flow in masked area
     img: original image
     gray: gray scale image
     flow: optical flow value array
-    active: target region
     typeNum: 1: one color 2: two color
     '''
-    x, y = active[:, 0], active[:, 1]
+    x, y = np.mgrid[step/2:WIDTH:step, step/2:HEIGHT:step].reshape(2,-1).astype(int)
     fx, fy = flow[y, x].T
     # change amount array
     opt_size = np.empty(len(fx))
@@ -58,51 +53,6 @@ def partical_draw_flow(img, gray, flow, active, typeNum=1):
             i += 1
     return img
 
-
-def count_pixcel(img, step):
-    '''
-    count target area cell in binary image by step interval
-    img: original image
-    step: step interval
-    '''
-    active = []
-    count = 0
-    for x in range(int(step / 2), WIDTH, step):
-        for y in range(int(step / 2), HEIGHT, step):
-            if img[y, x][0] == 255:
-                active.append([x, y])
-                count += 1
-    active = np.array(active).reshape(-1, 2)
-    return active, count
-
-
-def calc_mean(active, flow, num_pixcel):
-    '''
-    calculate mean (optical flow value) in target area
-    active: target area
-    num_pixcel: amount of pixcel in target area
-    '''
-    total = 0.0
-    for x, y in active:
-        total += ((flow[y, x][0])**2 + (flow[y, x][1]**2))**0.5
-    return (total / num_pixcel)
-
-
-def calc_val(active, flow, num_pixcel, mean):
-    '''
-    calculate variance (optical flow value) in target area
-    active: target area
-    flow: optical flow value array
-    num_pixcel: amount of pixcel in target area
-    mean: mean value in target area
-    '''
-    val = 0.0
-    for x, y in active:
-        val += (mean - (flow[y, x][0]**2 + flow[y, x][1]**2)**0.5)**2
-    val = val / num_pixcel
-    return val
-
-
 def show_gage(pointList, num):
     '''
     display loading gage
@@ -114,32 +64,54 @@ def show_gage(pointList, num):
         sys.stderr.write('\rWriting Rate:[{0}{1}] {2}%'.format(
             '*' * numIdx * 2, ' ' * (20 - numIdx), numIdx * 10))
 
+def calc_flow(prevGray, gray):
+    flow = cv2.calcOpticalFlowFarneback(prevGray,gray,None,0.5,3,15,3,5,1.2,0)
+    return flow
 
-def main(filePath, frameList):
-    global FOURCC
-    global FPS
+
+def calc_mean(maskFlow):
+    '''
+    calculate mean (optical flow value) in masked area
+    '''
+    mean = np.mean(maskFlow)
+    return mean
+
+
+def calc_val(maskFlow):
+    '''
+    calculate variance (optical flow value) in masked area
+    '''
+    var = np.var(maskFlow)
+    return var
+
+def load_mask(filepath):
+    mask = cv2.imread(filepath, 0)
+    mask = cv2.merge((mask, mask, mask))
+    mask[mask==255] = 1
+    return mask
+
+def main(filePath):
     global HEIGHT
     global WIDTH
-    global TOTALFRAME
     #-------------------------------------------------------
     # pre processing
     #------------------------------------------------------
     # capture movie and data
     cap = cv2.VideoCapture(filePath)
-    FOURCC = int(cv2.VideoWriter_fourcc(*'avc1'))
-    FPS = int(cap.get(cv2.CAP_PROP_FPS)/2)
+    fourcc = int(cv2.VideoWriter_fourcc(*'avc1'))
+    fps = int(cap.get(cv2.CAP_PROP_FPS)/2)
     HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    TOTALFRAME = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    totalFrame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     # preprocessing for loding gage
-    point = int(TOTALFRAME / 20)
+    point = int(totalFrame / 20)
     pointList = []
-    for i in range(point, TOTALFRAME, point):
+    for i in range(point, totalFrame, point):
         pointList.append(i)
     sys.stderr.write('\rWriting Rate:[{0}] {1}%'.format(' ' * 20, 0))
     # output movie
     #out = cv2.VideoWriter('../movie/out_' + filePath.split('/')[-1],
-    #FOURCC, FPS, (WIDTH, HEIGHT))
+    #fourcc, fps, (WIDTH, HEIGHT))
     # initial frame
     cap.set(cv2.CAP_PROP_POS_MSEC, 3 * 1000)
     ret, prev = cap.read()
@@ -147,11 +119,9 @@ def main(filePath, frameList):
     # average value list per frame
     meanList = []
     # variance value list per frame
-    valList = []
-    # store pixcel of the target area and their number
-    mask = cv2.imread('../image/image_data/mask.png', 0)
-    mask = cv2.merge((mask, mask, mask))
-    active, num_pixcel = count_pixcel(mask, 16)
+    varList = []
+    #load mask image
+    mask = load_mask('../image/image_data/mask.png')
 
     #-------------------------------------------------------
     # caluculate optical flow per frame
@@ -162,14 +132,15 @@ def main(filePath, frameList):
         frameNum += 1
         if ret == True:
             gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(prevGray,gray,None,0.5,3,15,3,5,1.2,0)
+            flow = calc_flow(prevGray, gray)
             prevGray = gray
-            mean = calc_mean(active,flow,num_pixcel)
+            maskFlow = flow*mask
+            mean = calc_mean(maskFlow)
             meanList.append(mean)
-            val = calc_val(active,flow,num_pixcel,mean)
-            valList.append(val)
+            var = calc_val(maskFlow)
+            varList.append(var)
             # make optical flow image
-            #flow_img = partical_draw_flow(img,gray,flow,active,1)
+            #flow_img = draw_flow(img, gray, maskFlow, 1)
             # restore and display
             #out.write(flow_img)
             show_gage(pointList,frameNum)
