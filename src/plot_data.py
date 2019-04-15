ac2848218369973e87a33c20bb8b8a8c568f5b37
kenya-sk <ksakka0309@gmail.com>
2019-04-15 21:01:57 +0900

    dev: done operation check

--- src/plot_data.py
+++ src/plot_data.py
@@ -3,36 +3,17 @@ import os
 import re
 import glob
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_graph(direc_path, normalize=False):
-    # read data from csv file and store it in list
-    def read_data(file_path):
-        value_lst = []
-        try:
-            with open(file_path, 'r') as f:
-                lines = f.readlines()
-        except FileNotFoundError:
-            print("Not Found {0} ! ".format(file_path))
-            sys.exit(1)
-
-        for line in lines:
-            value_lst.append(float(line.strip('\n')))
-
-        return value_lst
-
-    value_lst = []
-    file_path_lst = glob.glob(direc_path+"*.csv")
-    for path in file_path_lst:
-        value_lst.extend(read_data(path))
+def plot_graph(stas_file_path, conv_axis_path, save_path, title, normalize=False):
+    # read data of statistics value
+    value_lst = list(pd.read_csv(stas_file_path, header=None)[0])
 
     fig = plt.figure(figsize=(20, 6))
     ax = fig.add_subplot(1,1,1)
-
-    # title of graph ex) mean value (total)
-    statistic_type = str(direc_path.split('/')[-2])
-    plt.title(statistic_type + " value (total)")
+    plt.title(title)
 
     # set value of x axis
     value_x = []
@@ -40,7 +21,6 @@ def plot_graph(direc_path, normalize=False):
         value_x.append(i)
     value_x_lim_min = value_x[0]
     value_x_lim_max = value_x[-1]
-    print(len(value_x))
 
     if normalize:
         # normalize and set axis range to 0-1
@@ -51,41 +31,30 @@ def plot_graph(direc_path, normalize=False):
         value_y_lim_min = 0
         value_y_lim_max = max(value_lst)*1.01
 
-    value_y_lim_max = 150
     plt.xlim(value_x_lim_min, value_x_lim_max)
     plt.ylim(value_y_lim_min, value_y_lim_max)
-    plt.tick_params(labelsize=8)
+    plt.tick_params(labelsize=10)
     plt.xticks(rotation=90)
+    plt.xlabel("Frame Number")
+    plt.ylabel("Value [pixel]")
 
-    # one day data
-    ax.set_xticks([0, 8990, 17980, 26970, 35960, 44950,
-                    53939, 62929, 71919, 80909, 89899, 98889,
-                    107878, 116868, 125858, 134848, 143838, 152828,
-                    161817, 170807, 179797, 188787, 197777, 206767,
-                    215756, 224746, 233736, 242726, 251716, 260706,
-                    269696, 278686, 287676, 296666, 305656, 314646,
-                    323636, 332626, 341616, 350606, 359596, 368586,
-                    377576, 386566, 395556, 404546, 413536, 422526
-                    ])
-    ax.set_xticklabels(["09:00", "09:10", "09:20", "09:30", "09:40", "09:50",
-                        "10:00", "10:10", "10:20", "10:30", "10:40", "10:50",
-                        "11:00", "11:10", "11:20", "11:30", "11:40", "11:50",
-                        "12:00", "12:10", "12:20", "12:30", "12:40", "12:50",
-                        "13:00", "13:10", "13:20", "13:30", "13:40", "13:50",
-                        "14:00", "14:10", "14:20", "14:30", "14:40", "14:50",
-                        "15:00", "15:10", "15:20", "15:30", "15:40", "15:50",
-                        "16:00", "16:10", "16:20", "16:30", "16:40", "16:50"
-                        ])
+    # convert axis (frame number to something)
+    # ex) frame number to one day time (30 fps video)
+    if conv_axis_path is not None:
+        conv_df = pd.read_csv(conv_axis_path)
+        ax.set_xticks(conv_df["frame"])
+        ax.set_xticklabels(conv_df["time"])
 
     plt.grid(True)
     plt.plot(value_x, value_lst)
-    plt.savefig("../image/graph/2017-04-21/out_" + statistic_type + "_total.png" )
+    plt.savefig(save_path)
     plt.close(fig)
-    print("DONE: {}".format("/".join(direc_path.split('/')[-3:-1])))
+    print("SAVE: {0}".format(save_path))
 
 
 if __name__ == "__main__":
-    plot_graph("../data/2017-04-21/mean/", False)
-    plot_graph("../data/2017-04-21/var/", False)
-    plot_graph("../data/2017-04-21/max/", False)
-    plot_graph("../data/2017-04-21/human/", False)
+    conv_axis_path = None
+
+    plot_graph("../data/stats/mean.csv", conv_axis_path, "../data/graph/mean.eps", "mean graph", False)
+    plot_graph("../data/stats/var.csv", conv_axis_path, "../data/graph/var.eps", "var graph", False)
+    plot_graph("../data/stats/max.csv", conv_axis_path, "../data/graph/max.eps", "max graph", False)
